551e29f73f63c58915c660aa6a42cca802532749
Yash Shah <yashshah1282@gmail.com>
2022-10-29 09:54:41 +0300

    Added Inputs to calculate age / bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AgeCalculator';
+  
+  @ViewChild('dobInput') dobInput!: ElementRef;
+  @ViewChild('ageInput') ageInput!: ElementRef;
+
+  calculateAge() {
+    const birthYear = new Date(this.dobInput.nativeElement.value).getFullYear();
+    const currentYear = new Date().getFullYear();
+    let age = currentYear - birthYear;
+    this.ageInput.nativeElement.value = age;
+  }
 }
